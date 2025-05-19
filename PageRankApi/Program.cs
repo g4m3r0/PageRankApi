83a94862ee836074a65bf15d50f3547c64da58a6
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2025-05-19 14:58:53 -0400

    feat: We want to always show the openapi docs and swagger ui (at least for now)

--- PageRankApi/Program.cs
+++ PageRankApi/Program.cs
@@ -20,12 +20,9 @@ public class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.MapOpenApi();
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.MapOpenApi();
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
 
